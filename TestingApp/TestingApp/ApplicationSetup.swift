5d15bc8d9cec407ea0d0415a66d9e196379d7360
Jonathan Pappas <36423382+JonnyGamer@users.noreply.github.com>
2025-03-08 12:13:33 -0700

    testing app

---
+++ TestingApp/TestingApp/ApplicationSetup.swift
@@ -0,0 +1,19 @@
+//
+//  ApplicationSetup.swift
+//  TestingApp
+//
+//  Created by Jonathan Pappas on 3/8/25.
+//
+
+import SwiftUI
+import SuperKit
+
+@main
+struct YourApp: App {
+    var body: some SwiftUI.Scene {
+        WindowGroup {
+            Build(MainScene())
+        }
+    }
+}
+
---
+++ TestingApp/TestingApp/MainScene.swift
@@ -0,0 +1,19 @@
+//
+//  MainScene.swift
+//  TestingApp
+//
+//  Created by Jonathan Pappas on 3/8/25.
+//
+
+import SuperKit
+
+class MainScene: Scene {
+
+    func began() {
+        let box = Box(width: 100, height: 100)
+        box.x = 800
+        box.y = 500
+    }
+
+}
+
